c1a7e3499735cf9de50d44638a984b2c1398f66a
Divij Sasidhar <divijsasi@gmail.com>
2024-02-20 13:28:05 -0500

    min. viable product

--- spotifygrabber.py
+++ spotifygrabber.py
@@ -1,8 +1,9 @@
 import spotipy
-from time import sleep
 import requests
 from msvcrt import getch
 from spotipy.oauth2 import SpotifyClientCredentials
+from os import mkdir, getcwd
+import csv
 
 print("Creator: Divij Sasidhar & Arnav Sahni")
 print("NOT FOR REPRODUCTION WITHOUT AUTHORIZATION")
@@ -14,7 +15,7 @@ class Track:
         self.name = name
         self.song_uri = uri
         self.artist_name = artist_name
-        self.duration = duration
+        self.duration = duration / 1000
         self.album_cover = album_cover
 
     def __str__(self):
@@ -29,7 +30,6 @@ with open('config.txt', 'r') as f:
     client_secret = f.readline().split(' ')[1]
 
 spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id, client_secret))
-
 
 name = (input("Enter Song Title: "))
 results = spotify.search(q=name, type='track')
@@ -55,23 +55,44 @@ for track_number in results:
 
 print("\nEnter the number of the track you'd like to print: ", end="")
 track = tracks[int(getch())]
-
+length_min, length_sec = int(track.duration / 60), str(int((track.duration % 60))).zfill(2)
 
 print(f"\nPress [Enter] to confirm printing of {track.name} by", track.artist_name[0])
 if getch() != b'\r':
-    print("Quitting program...")
-    sleep(3)
     exit()
 
-print("Saving files...")
+chosen_min = input("Enter chosen time minutes (example: if it's 1:03, enter 1): ")
+chosen_sec = input("Enter chosen time seconds (example: if it's 1:03, enter 03): ")
+
+path = getcwd() + '\\' f'{track.name}\\'
+mkdir(path)
+print(f"Saving files in {path}...")
 
 # spotify code
-url = f"https://scannables.scdn.co/uri/plain/png/000000/white/640/{track.song_uri}"
+url = f"https://scannables.scdn.co/uri/plain/png/FFFFFF/black/640/{track.song_uri}"
 data = requests.get(url).content
-with open(f'{track.name}_spotify_code.png', 'wb') as f:
+with open(path + f'spotify_code.png', 'wb') as f:
     f.write(data)
 
 # album cover
 data = requests.get(track.album_cover).content
-with open(f'{track.name}_album_cover.png', 'wb') as f:
+with open(path + f'album_cover.png', 'wb') as f:
     f.write(data)
+
+data = {'SongTitle': track.name,
+        'ArtistName': ', '.join(track.artist_name),
+        'LengthMinutes': length_min,
+        'LengthSeconds': length_sec,
+        'ChosenMinutes': chosen_min,
+        'ChosenSeconds': chosen_sec,
+        '@SongCode': path + f'spotify_code.png',
+        '@AlbumCover': path + f'album_cover.png'
+        }
+
+with open(path + f'{track.name}.csv', 'w', newline="") as f:
+    writer = csv.writer(f)
+    w = csv.writer(f)
+    w.writerows((data.keys(), data.values()))
+print("Saved!")
+print("Program finished, press any key to exit.")
+getch()
